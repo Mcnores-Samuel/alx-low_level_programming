94e156312b45c7bbe189520303108f5a7facd4c7
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-02-22 16:49:26 +0200

    printing the last digit in style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,29 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the absolute last digit of a number.
+ * _abs - returns the absolute number.
+ * @num: the number to be checked for formatting.
+ * Return:absolute number.
+ */
+int _abs(int num)
+{
+if (num < 0)
+{
+return (-num);
+}
+else
+{
+return (num);
+}
+}
+
+/**
+ * print_last_digit - returns the absolute last digit of a number.
  * @num: the number to be checked for formatting.
  * Return: the last digit of a number.
  */
-
 int print_last_digit(int num)
 {
 int last = num % 10;
-return _abs(last);
+return (_abs(last));
 }
