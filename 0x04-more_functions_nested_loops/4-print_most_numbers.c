b55f1d9cee6fec32bd0b39eddeb2bc6d2fe30cf5
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-02-23 22:29:23 +0200

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,11 @@ void print_most_numbers(void)
 int num;
 for (num = 48; num <= 57; num++)
 {
-if (num != 50 || num != 52)
+if (num == 50 || num == 52)
 {
-_putchar(num);
+continue;
 }
+_putchar(num);
 }
 _putchar('\n');
 }
