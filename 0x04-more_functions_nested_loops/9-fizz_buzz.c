fe731ee4162cfcdd5fafb9eeb7dbed0ea148bcb9
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-02-24 00:29:52 +0200

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ for (x = 1; x <= size; x++)
 {
 if ((x % 3 == 0) && (x % 5 == 0))
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
+printf(" ");
 }
 else if (x % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
+printf(" ");
 }
 else if (x % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
+printf(" ");
 }
-else 
+else
 {
-printf("%d ", x);
+printf("%d", x);
+printf(" ");
 }
 }
 printf("\n");
