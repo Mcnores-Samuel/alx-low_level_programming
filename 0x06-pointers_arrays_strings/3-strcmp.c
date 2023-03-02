7e95c899f98215c5958d05549365f39a5ddca19a
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-03 01:29:04 +0200

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,10 +17,10 @@ int _strcmp(char *s1, char *s2)
 	}
 	if (s1[i] == s2[i])
 	{
-		return s1[i] - s2[i];
+		return (s1[i] - s2[i]);
 	}
 	else
 	{
-		return s1[i] - s2[i];
+		return (s1[i] - s2[i]);
 	}
 }
