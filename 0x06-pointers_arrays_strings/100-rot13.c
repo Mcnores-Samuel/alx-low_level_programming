5f9677635bda87e3ad41054e9ff20b757e381401
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-03 14:05:54 +0200

    scrumble rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+char *rot13(char *str)
+{
+	int i, shift;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (isalpha(str[i]))
+		{
+			shift = isupper(str[i]) ? 'A' : 'a';
+			str[i] = (str[i] - shift + 13) % 26 + shift;
+		}
+	}
+	return str;
+}
