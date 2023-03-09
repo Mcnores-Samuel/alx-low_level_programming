5c3d49b4ee7fb484eba539f5fb94ec2578925a6c
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-09 04:39:43 +0200

    The only way to solve this problem is to make sure the solution runs in recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * prime -  checks if a given number "n" is a prime number.
+ * @divisor: the divisor being tested
+ * @n: the number being tested for primality.
+ * Return: 0 if n is not a prime or 1 if true.
+ */
+
+int isPrime(int divisor, int n)
+{
+	if (n < 2 || !(n % divisor))
+		return (0);
+	else if (divisor > n / 2)
+		return (1);
+	else
+		return (isPrime(divisor + 1, n));
+}
+
+/**
+ * is_prime_number - check for prime numbers
+ * @n: the number being tested
+ * Return: 1 if the n is prime or 0 if not a prime number.
+ */
+
+int is_prime_number(int n)
+{
+	return (isPrime(2, n));
+}
