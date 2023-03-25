8ac8259c2e0dfb3ec034ceddfb85b827a98c66ed
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-26 01:52:16 +0200

    allocate memory and testing

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * declare a pointer to the memory block to be allocated;
+ * declare necessary helping variables
+ * use strlen to determine length of the first string
+ * the second string length will be determined by the n byte
+ * add first string length + n byte value to get the full length of
+ * the resulting string and use the added to allocate the memory
+ * use the loop i iterating through the first string and when done move the curser to the end
+ * then iterate the second string and assign iterated values to the heap memory
+ * 
+ * 
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *ptr;
+    int len = strlen(s1) + n, i, j;
+
+    ptr = (char *)calloc(len, sizeof(char));
+
+    if (ptr == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; s1[i] != '\0'; i++)
+    {
+        ptr[i] = s1[i];
+    }
+
+    while (j < n)
+    {
+        ptr[i + j] = *s2++;
+        j++;
+    }
+
+    return (ptr);
+}
