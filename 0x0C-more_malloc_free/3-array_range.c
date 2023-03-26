225bb0718d06b309af97aa093c3425cc2c2b89f0
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-26 04:13:19 +0200

    allocate memory and testing

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: mimimum number element of the array
+ * @max: maxmum number element of the array
+ * Return: pointer to the newly created array.
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, j = 0;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = (int *)malloc((max - min + 1) * sizeof(int));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for(i = min; i >= min && i <= max; i++)
+	{
+		ptr[j] = i;
+		j++;
+	}
+	return (ptr);
+}
