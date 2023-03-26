45ce6a52deb8bab27e6f676be1a1e44ff445bc7a
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-26 21:24:42 +0200

    printing structure

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int x, y;
+
+	if (name != NULL || owner != NULL)
+	{
+		dog = malloc(sizeof(dog_t));
+
+		if (dog == NULL)
+		{
+			return (NULL);
+		}
+
+		x = strlen(name) + 1;
+		y = strlen(owner) + 1;
+
+		dog->name = malloc(sizeof(char) * (x));
+
+		if (dog->name == NULL)
+		{
+			free(dog);
+		}
+
+		dog->name = strcpy(dog->name, name);
+		dog->owner = strcpy(dog->owner, owner);
+		dog->age = age;
+		
+	}
+	return (dog);
+}
