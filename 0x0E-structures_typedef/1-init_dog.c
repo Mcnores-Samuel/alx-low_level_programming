204048082c0fc9f8373550ba21dc14eb97370335
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-26 20:10:16 +0200

    initializing the structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,17 @@
  * @name: pointer to the name struct element.
  * @age: age of the dog
  * @owner: pointer to the owner struct element.
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
+		return;
 }
