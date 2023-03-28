f738535fd146a80152202032472961494ac3f0eb
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-28 12:29:47 +0200

    sum all the parameters

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,14 +13,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
-	va_start(args, n);
+	char *str;
 
 	if (n > 0)
 	{
+		va_start(args, n);
+
 		for (i = 0; i < n; i++)
 		{
-			char *str = va_arg(args, char *);
+			str = va_arg(args, char *);
 
 			if (str == NULL)
 				printf("(nil)");
