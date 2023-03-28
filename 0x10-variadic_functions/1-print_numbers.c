93d5f322078879e3d61c773170952f523aefe8be
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-03-28 11:12:54 +0200

    sum all the parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers seperated by the seperator.
+ * @seperator: the seperator to seperate the numbers
+ * @n: the number of integers passed to the function.
+ * Description: the function takes as many parameters as desired
+ * and will be printed to the console seperated by the seperator.
+ * Return: void / nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg_ptr;
+
+	va_start(arg_ptr, n);
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			int next = va_arg(arg_ptr, int);
+
+			if (separator)
+			{
+				printf("%d", next);
+
+				if (i != n -1)
+				{
+					printf("%s", separator);
+				}
+			}
+			else
+				printf("%d", next);
+		}
+		printf("\n");
+	}
+}
