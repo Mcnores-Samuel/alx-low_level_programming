9e5584ff1972f3c6a6dc9d26fdf88d97d89db760
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-04-12 13:07:51 +0200

    appends text on the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_d = open(filename, O_APPEND);
+	file_d = open(filename, O_APPEND | O_WRONLY);
 
 	if (file_d == -1)
 		return (-1);
