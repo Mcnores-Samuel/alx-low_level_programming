8fc9e17032728c0121691649fddd31b703bd4fa8
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-04-12 12:45:19 +0200

    creates a file with rw------

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,11 @@
 #include "main.h"
-#include <string.h>
 
+/**
+ * create_file - craetes a file if it doesn't exist.
+ * @filename: pointer to the file to be created.
+ * @text_content: data or text to write to the file.
+ * Return: 1 on success or -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int file_d, write_d, str_len = 0;
