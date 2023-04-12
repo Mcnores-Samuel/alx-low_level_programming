3ab1d3a997b98221076aa5f7dc8212ad9621772e
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-04-12 12:41:16 +0200

    creates a file with rw------

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,23 +3,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_d, write_d;
-	int buffer;
-
-	buffer = strlen(text_content);
-
-	if (filename && text_content != NULL)
-	{
-		file_d = open(filename, O_TRUNC);
-		if (file_d == -1)
-		{
-			close(file_d);
-			return (-1);
-		}
-
-		write_d = write(file_d, text_content, buffer);
-		close(file_d);
-	}
+	int file_d, write_d, str_len = 0;
 
 	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_d == -1)
@@ -28,11 +12,18 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	write_d = write(file_d, text_content, buffer);
-	if (write_d == -1)
+	if (text_content != NULL)
 	{
+		while (text_content[str_len] != '\0')
+			str_len++;
+
+		write_d = write(file_d, text_content, str_len);
+		if (write_d == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
 		close(file_d);
-		return (-1);
 	}
 	return (1);
 }
