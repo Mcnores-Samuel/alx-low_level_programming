03154de1cb0d663d10b9386d288b96e2b64c042f
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-04 02:27:39 +0200

    fixed deleted node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,15 +31,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			i++;
 		}
 
+		if (current_node == NULL || current_node->next == NULL)
+			return (-1);
+
 		tmp = current_node->next;
 		current_node->next = tmp->next;
-		if (tmp != NULL)
-		{
-			free(tmp);
-			return (1);
-		}
-		else
-			return (-1);
+		free (tmp);
 	}
 	return (-1);
 }
