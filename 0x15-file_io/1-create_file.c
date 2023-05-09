a4e329a1070eb18150f3ef0cea0503e5e45675a8
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-09 03:30:06 +0200

    creates and write to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,31 +3,30 @@
 /**
  * create_file - craetes a file if it doesn't exist.
  * @filename: pointer to the file to be created.
- * @text_content: data or text to write to the file.
+ * @text_content: pointer to data or text to write to the file.
  * Return: 1 on success or -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d, write_d, str_len = 0;
+	int file_d, bytes = 0;
 
-	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_d == -1)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
+	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (text_content != NULL)
+	if (file_d == -1)
+		return (-1);
+
+	while (text_content[bytes] != '\0' && text_content != NULL)
 	{
-		while (text_content[str_len] != '\0')
-			str_len++;
-
-		write_d = write(file_d, text_content, str_len);
-		if (write_d == -1)
+		if (write(file_d, &text_content[bytes], 1) == -1)
 		{
 			close(file_d);
 			return (-1);
 		}
-		close(file_d);
+		bytes++;
 	}
+	close(file_d);
 	return (1);
 }
+
