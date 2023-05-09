f62f435b7350535e1b24ee0807bd978209e6532a
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-09 02:11:27 +0200

    read from a file and print to stdin

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "file.h"
+
+/**
+ * read_textfile - reads text and prints it to the POSIX standard output
+ * @filename: pointer to the file to read from.
+ * @letters: number of bytes or letters to read from the file.
+ * Return: number of bytes read and printed or 0 if error occured.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int file_d, bytes = 0;
+	char *data;
+
+	if (filename == NULL)
+		return (0);
+
+	if (letters > 0)
+	{
+		data = (char *)malloc(sizeof(char) * letters);
+		if (data == NULL)
+			return (0);
+	}
+
+	if ((file_d = open(filename, O_RDONLY)) == -1)
+		return (0);
+	if (read(file_d, data, letters) == -1)
+	{
+		close(file_d);
+		return (0);
+	}
+	while (data[bytes] != EOF && bytes < letters)
+	{
+		write(STDIN_FILENO, &data[bytes], 1);
+		bytes++;
+	}
+	close(file_d);
+	free(data);
+	return (bytes);
+}
