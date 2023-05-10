0190b142892d05acd9f0f4ed86d9d6703b4cf824
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-10 08:39:11 +0200

    appends text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,33 +2,33 @@
 
 /**
  * append_text_to_file - appends text at the end of the file.
- * @filename: pointer to the file to append text.
- * @text_content: pointer to the text to be appended.
+ * @filename: pointer to the file to append text or data to.
+ * @text_content: pointer to the text or data to be appended.
  * Return: 1 on success of -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_d, written_d, str_len = 0;
+	int file_d, bytes = 0;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	file_d = open(filename, O_APPEND | O_WRONLY);
 
 	if (file_d == -1)
 		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[str_len] != '\0')
-			str_len++;
-		written_d = write(file_d, text_content, str_len);
 
-		if (written_d == -1)
-		{
-			close(file_d);
-			return (-1);
-		}
+	while (text_content[bytes] != '\0')
+		bytes++;
+
+	bytes = write(file_d, text_content, bytes);
+
+	if (bytes == -1)
+	{
 		close(file_d);
+		return (-1);
 	}
+
+	close(file_d);
 	return (1);
 }
