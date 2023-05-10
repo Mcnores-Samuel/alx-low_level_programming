57d0296975ada9759feca5e7408ee9c8aca7d861
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-10 08:41:52 +0200

    appends text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,25 +10,25 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file_d, bytes = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	file_d = open(filename, O_APPEND | O_WRONLY);
 
 	if (file_d == -1)
 		return (-1);
-
-	while (text_content[bytes] != '\0')
-		bytes++;
-
-	bytes = write(file_d, text_content, bytes);
-
-	if (bytes == -1)
+	if (text_content != NULL)
 	{
-		close(file_d);
-		return (-1);
-	}
+		while (text_content[bytes] != '\0')
+			bytes++;
+		bytes = write(file_d, text_content, bytes);
 
-	close(file_d);
+		if (bytes == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
+		close(file_d);
+	}
 	return (1);
 }
