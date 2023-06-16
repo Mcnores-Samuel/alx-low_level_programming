94ef092287f5f2cee50364d2a48b324f6fc97358
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-16 11:46:22 +0200

    returns the nth node of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,6 +3,7 @@
 /**
  * get_dnodeint_at_index - gets a node at a position index.
  * @head: pointer to the head node of the dlistint_t list.
+ * @index: position at which to return the node pointer
  * Return: pointer to the node at position index or NULL otherwise.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
@@ -10,15 +11,15 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	size_t n = 0;
 
 	if (index == 0)
-		return head;
+		return (head);
 
 	while (head != NULL)
 	{
 		if (n == index - 1)
-			return head;
+			return (head);
 		n++;
 		head = head->next;
 	}
 
-	return NULL;
+	return (NULL);
 }
