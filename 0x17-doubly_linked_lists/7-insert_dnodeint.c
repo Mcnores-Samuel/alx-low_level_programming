e636ae6918f485e706ecc152ae3392a830efa267
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-17 01:59:33 +0200

    inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,33 +11,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current, *temp;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	current = *h;
+	while (idx != 1)
+	{
+		current = current->next;
+		if (current == NULL)
+			return (NULL);
+		idx--;
+	}
+
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	if (*h == NULL)
-	{
-		new_node->next = *h;
-		(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
-	current = *h;
-	while (current != NULL && idx >= 0)
-	{
-		current = current->next;
-		idx--;
-	}
 	temp = current->next;
-	if (current != NULL)
-	{
-		current->next = new_node;
-		new_node->next = temp;
-		temp->prev = new_node;
-		new_node->prev = current;
-		return (new_node);
-	}
-	return (NULL);
+	current->next = new_node;
+	new_node->next = temp;
+	temp->prev = new_node;
+	new_node->prev = current;
+
+	return (new_node);
 }
