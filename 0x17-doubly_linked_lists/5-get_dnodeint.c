0e145a13e099334ede17c26ed6d712d85fc41665
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-16 11:28:54 +0200

    dlistint_t *new_node = NULL;

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets a node at a position index.
+ * @head: pointer to the head node.
+ * Return: pointer to the node at position index or NULL otherwise.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t n = 0;
+
+	for (n = 0; head != NULL && n < index; n++)
+	{
+		if (n == index - 1 && head)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
