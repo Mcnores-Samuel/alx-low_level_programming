1bda555992c912d76d491f4de85627eba256f8fb
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-07 17:58:31 +0200

    craete a hash value

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hashes a key and returns a unique number to be used for
+ * positioning a key value pair in the hash table array.
+ * @str: key or str to hash
+ * Return: a unique integer known as a hash value.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int chr;
+
+	hash = 5381;
+	while((chr = *str++))
+		hash = ((hash << 5) + hash) + chr;
+	return (hash);
+}
