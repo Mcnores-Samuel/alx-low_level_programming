e1d3a64c567c4f29cf4f78b35a9fb5e69b0ed881
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-07 22:56:19 +0200

    gets value for the key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table to look for the value associated with the key.
+ * @key: the key to search for in the hash table.
+ * Return: the value associated with the element, or
+ * NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int pos;
+	hash_node_t *current;
+	char *value;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	pos = key_index((unsigned char *)key, ht->size);
+
+	current = ht->array[pos];
+
+	if (current != NULL && strcmp(current->key, key) == 0)
+		value = current->value;
+	else
+		value = NULL;
+	return (value);
+}
