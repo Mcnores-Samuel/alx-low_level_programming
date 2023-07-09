8b1d49d93e44fe530b2a29ae3e65302f1c98a839
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-10 00:07:12 +0200

    sorted hash table

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -19,3 +19,35 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 
 	return (position);
 }
+
+/**
+ * _strdup - duplicates a string.
+ * @str: string to duplicate.
+ * Return: pointer to the duplicate string.
+*/
+char *_strdup(char *str)
+{
+	char *dest;
+	int len = 0, i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = strlen(str);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
