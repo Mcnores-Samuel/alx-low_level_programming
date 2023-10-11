c3024351faa9ab4275ade16533afbf99d0930a77
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-10-12 01:13:55 +0200

    searches for a value in a sorted array of integers.

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,65 @@
+#include "search_algos.h"
+
+
+/**
+ * search_algorithm - searches for a given value in an array using
+ * binary_search algorithm.
+ * @array: is a pointer to the first element of the array to search in
+ * @size: size is the number of elements in array
+ * @left: the lesser most position in the array
+ * @right: the greater most position in the array
+ * @search_value: a value to be searched in the array
+ * Return: an index of the value in the array or -1 otherwise
+ */
+int search_algorithm(int *array, size_t size,
+		int left, int right, int search_value)
+{
+	int mid_point;
+	int trav_from;
+
+	if (right >= left)
+	{
+		trav_from = left;
+
+		printf("Searching in array: ");
+		while (trav_from <= right)
+		{
+			printf("%d", array[trav_from]);
+			if (trav_from != right)
+				printf(", ");
+			trav_from++;
+		}
+		printf("\n");
+		mid_point = left + (right - left) / 2;
+
+		if (array[mid_point] == search_value &&
+				(mid_point == left || array[mid_point - 1]
+				 != search_value))
+			return (mid_point);
+
+		if (array[mid_point] < search_value)
+			return (search_algorithm(array, size,
+						mid_point + 1, right, search_value));
+		return (search_algorithm(array, size, left,
+					mid_point - 1, search_value));
+	}
+	return (-1);
+}
+
+/**
+ * advanced_binary - invokes search_algorithm function to perform a
+ * search.
+ * @array: a is a pointer to the first element of the array to search in
+ * @size: size is the number of elements in array
+ * @value: value to be searched in the array
+ * Return: index of the value searched or -1 otherwise
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int index;
+
+	if (array == NULL)
+		return (-1);
+	index = search_algorithm(array, size, 0, size - 1, value);
+	return (index);
+}
